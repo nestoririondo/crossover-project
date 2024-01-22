11e30902947611f90799e7e0a04570d37fc07d49
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-22 15:14:18 +0100

    Daily objective items className toggle  green/red

--- src/components/TotalResults.jsx
+++ src/components/TotalResults.jsx
@@ -5,20 +5,6 @@ import { NutritionContext } from "../provider/NutritionContext";
 const TotalResults = () => {
   const { formattedResults, allSelections } = useContext(NutritionContext);
 
-  console.log("From TotalResults:", formattedResults);
-  console.log("From TotalResults:", allSelections);
-
-  const dailyObjective = {
-    calories: 2000,
-    carbs: 200,
-    protein: 100,
-    fat: 50,
-    age: 30,
-    weight: 70,
-    height: 170,
-    gender: "female",
-  };
-
   const calculateTotal = (meals, nutrient) => {
     return meals.reduce((acc, meal) => acc + meal[nutrient], 0).toFixed(2);
   };
@@ -27,23 +13,17 @@ const TotalResults = () => {
   const totalCarbs = calculateTotal(formattedResults, "carbs");
   const totalProtein = calculateTotal(formattedResults, "protein");
   const totalFat = calculateTotal(formattedResults, "fat");
-
-  const calcBMR = (allSelections) => {
-    return allSelections.gender === "male"
-      ? 66.5 +
-          13.75 * allSelections.weight +
-          5 * allSelections.height -
-          6.75 * allSelections.age
-      : 655.1 +
-          9.563 * allSelections.weight +
-          1.85 * allSelections.height -
-          4.676 * allSelections.age;
+  const consumed = {
+    totalCalories,
+    totalCarbs,
+    totalProtein,
+    totalFat,
   };
 
   const calcProtein = (allSelections) => {
     return {
-      min: (allSelections.weight * 1.4).toFixed(2),
-      max: (allSelections.weight * 2.4).toFixed(2),
+      min: (allSelections.weight * 1.2).toFixed(2),
+      max: (allSelections.weight * 2.2).toFixed(2),
     };
   };
 
@@ -61,15 +41,24 @@ const TotalResults = () => {
     };
   };
 
+  const calcBMR = (allSelections) => {
+    return allSelections.gender === "male"
+      ? 66.5 +
+          13.75 * allSelections.weight +
+          5 * allSelections.height -
+          6.75 * allSelections.age
+      : 655.1 +
+          9.563 * allSelections.weight +
+          1.85 * allSelections.height -
+          4.676 * allSelections.age;
+  };
+
   const dailyProtein = calcProtein(allSelections);
   const dailyFat = calcFat(allSelections);
   const dailyCarbs = calcCarbs(allSelections);
   const bmr = calcBMR(allSelections).toFixed(2);
 
-  // Men: BMR = 66.5 + (13.75 x weight in kilos) + (5 x height in cm) – (6.75 x age in years)
-  // Women: BMR = 655.1 + (9.563 X weight in kilos) + (1.85 X height in cm) – (4.676 x age in years)
-  // Source: https://www.bmi-calculator.net/bmr-calculator/
-
+  console.log(totalCalories, allSelections.calories);
   return (
     <div className="totalresults">
       <div className="consumed">
@@ -80,11 +69,51 @@ const TotalResults = () => {
         <div>{totalFat} g fat</div>
       </div>
       <div className="objective">
-        <div>Your daily objective</div>
-        <div>{allSelections.calories}</div>
-        <div>{allSelections.carbs}</div>
-        <div>{allSelections.proteine}</div>
-        <div>{allSelections.fat}</div>
+        <div className="your-daily">Your daily objective</div>
+        {allSelections.calories && (
+          <div
+            className={
+              Number(totalCalories) < Number(allSelections.calories)
+                ? "green"
+                : "red"
+            }
+          >
+            {allSelections.calories} <span>kCal</span>
+          </div>
+        )}
+        {allSelections.carbs && (
+          <div
+            className={
+              Number(totalCarbs) < Number(allSelections.carbs)
+                ? "green"
+                : "red"
+            }
+          >
+            {allSelections.carbs} <span>g carbs</span>
+          </div>
+        )}
+        {allSelections.proteine && (
+          <div
+            className={
+              Number(totalProtein) < Number(allSelections.proteine)
+                ? "green"
+                : "red"
+            }
+          >
+            {allSelections.proteine} <span>g protein</span>
+          </div>
+        )}
+        {allSelections.fat && (
+          <div
+            className={
+              Number(totalFat) < Number(allSelections.fat)
+                ? "green"
+                : "red"
+            }
+          >
+            {allSelections.fat} <span>g fat</span>
+          </div>
+        )}
       </div>
       <div className="ideal">
         <div>Your ideal daily intake</div>
