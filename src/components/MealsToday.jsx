8451c40cdfe4cc6f1b12aa3386860ebb0057113a
kenkindermann <ken.kindermann@gmail.com>
2024-01-22 11:48:37 +0100

    allMeals is created by searchInputs, data is formatted for totalResults

--- src/components/MealsToday.jsx
+++ src/components/MealsToday.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { NutritionContext } from "../provider/NutritionContext";
 
 const MealsToday = () => {
-  const { allNutritions, setAllNutritions } = useContext(NutritionContext);
-  const results = {
-    totalFat: 0,
-    totalCarbs: 0,
-    totalProtein: 0,
-    totalCalories: 0,
-  };
+  const { allNutritions, setAllNutritions, search } = useContext(NutritionContext);
+  const [allMeals, setAllMeals] = useState([]);
+  const [formattedResults, setFormattedResults] = useState([]);
 
+  useEffect(() => {
+    setAllMeals([...allMeals, ...search]);
+  }, [search]);
 
+  useEffect(() => {
+    const dataForTotalResults = allMeals.map((meal, index) => ({
+      id: index,
+      calories: meal.calories,
+      carbs: meal.carbohydrates_total_g,
+      protein: meal.protein_g,
+      fat: meal.fat_total_g,
+    }));
+
+    console.log(dataForTotalResults);
+    setFormattedResults(dataForTotalResults);
+  }, [allMeals]);
+
+  useEffect(() => {
+    console.log("FORMATTED", formattedResults);
+  }, [formattedResults]);
 
   return <div>MealsToday</div>;
 };
